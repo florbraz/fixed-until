c156007e6cfb961b4806f3545faf635749ad44da
florbraz <eu@florbraz.com>
2014-05-20 18:21:10 +0200

    Was having problems on resetting the style for mobile

--- fixed-until.js
+++ fixed-until.js
@@ -1,41 +1,42 @@
-$(window).on('load resize', function(e) {
-	var evt = e.type;
+$(window).on('scroll load resize focus', function(e) {
 	
 	$('[data-fixed-limit]').attr('style', '');
 
 	if ($(window).width() >= 768) {
 
 		$('[data-fixed-limit]').each(function() {
+
 			if ($(window).width() >= 1280) {
 				$(this).width($(this).width());
 			}
+			
 			var headerHeight = $('.topbar').outerHeight();
-			var $this = $(this);
 			var limitTop = $(this).offset().top;
 			var limitElement =  $(this).attr('data-fixed-limit');
 			var limitHeight = $(limitElement).height();
 			var limitBottom = $(limitElement).offset().top;
-			var elementHeight = $(this).height();
-			
-			$(window).on('scroll resize', function() {	
-				var scrollTop = $(window).scrollTop();
-				if (scrollTop + headerHeight >= limitTop)  {
+			var elementHeight = $(this).height();			
+			var scrollTop = $(window).scrollTop();
+			var $this = $(this);
+
+			if (scrollTop + headerHeight >= limitTop)  {
+				$this.css({
+					'position':'fixed'
+				});
+				if (scrollTop + elementHeight + headerHeight <= limitHeight + limitBottom ) {
 					$this.css({
-						'position':'fixed'
+						'top': headerHeight
 					});
-					if (scrollTop + elementHeight + headerHeight <= limitHeight + limitBottom ) {
-						$this.css({
-							'top': headerHeight
-						});
-					} else {
-						$this.css({
-							'top': limitHeight - scrollTop - elementHeight  + limitTop
-						});	
-					}
 				} else {
-					$this.attr('style', '');
+					$this.css({
+						'top': limitHeight - scrollTop - elementHeight  + limitTop
+					});	
 				}
-			});
+			} else {
+				$this.attr('style', '');
+			}
+
 		});
-	}
+	} 
+	
 });
